d1c65fc1f0472b21d333cad4d9d88707b336043d
Brian Denicola <bjd145@yahoo.com>
2019-02-22 08:58:24 -0600

    Additional steps to get pub/pri key auth working

--- scripts/configure-sshd.ps1
+++ scripts/configure-sshd.ps1
@@ -1,6 +1,7 @@
 $publickey = ""
 $sshRoot = Join-Path -Path $ENV:USERPROFILE -ChildPath ".ssh"
 $authorizedFile = Join-Path -Path $sshRoot -ChildPath "authorized_keys"
+$ssh_config = "C:\ProgramData\ssh\sshd_config"
 
 New-Item -Path $sshRoot -ItemType Directory
 Out-File -FilePath $authorizedFile -Encoding ascii -InputObject $publickey
@@ -14,13 +15,19 @@ if($kb) {
 
     Set-Service -Name ssh-agent -StartupType Automatic
     Set-Service -Name sshd -StartupType Automatic
+    New-NetFirewallRule -DisplayName "SSH (Server)" -Protocol TCP -LocalPort 22 -Action Allow 
 
     Start-Service ssh-agent
+    Repair-AuthorizedKeyPermission $authorizedFile -Confirm:$false
+    icacls $authorizedFile /remove "NT SERVICE\sshd"
+    
+    $sshd = Get-Content -Path $ssh_config -Raw
+    $sshd = $sshd.Replace("Match Group administrators", "#Match Group administrators")
+    $sshd = $sshd.Replace("       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys","#       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys")^C
+    $sshd | Out-File -Encoding ascii -FilePath $ssh_config
     Start-Service sshd
-
-    New-NetFirewallRule -DisplayName "SSH (Server)" -Protocol TCP -LocalPort 22 -Action Allow 
 }
 else {
-    #Get-WUInstall -KBid KB4480116
+    #Get-WUInstall -KBArticleID KB4480116
     Write-Output "KB4480116 is required to be installed first."
 }
