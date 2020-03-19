fb667d066018e6ee690a91cc2352d9d2eab4de02
Brian Denicola <bjd145@yahoo.com>
2020-03-19 09:59:40 -0500

    Removed Dependency on OpenSSHUtils

--- scripts/configure-sshd.ps1
+++ scripts/configure-sshd.ps1
@@ -14,21 +14,21 @@ Out-File -FilePath $authorizedFile -Encoding ascii -InputObject $publickey
 
 $ssh_regKey = "HKLM:\SOFTWARE\OpenSSH"
 $ssh_config = "C:\ProgramData\ssh\sshd_config"
-$pwsh_path  = Get-Command -Name Powershell | Select -ExpandProperty Source
+$pwsh_path  = Get-Command -Name Powershell | Select-Object -ExpandProperty Source
 
 New-ItemProperty -Path $ssh_regKey -Name DefaultShell -Value $pwsh_path -PropertyType String -Force 
 
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
-Install-Module -Force OpenSSHUtils -Scope AllUsers
+#Install-Module -Force OpenSSHUtils -Scope AllUsers
 
 Set-Service -Name ssh-agent -StartupType Automatic
 Set-Service -Name sshd -StartupType Automatic
 New-NetFirewallRule -DisplayName "SSH (Server)" -Protocol TCP -LocalPort 22 -Action Allow 
 
 Start-Service ssh-agent
-Repair-AuthorizedKeyPermission $authorizedFile -Confirm:$false
-icacls $authorizedFile /remove "NT SERVICE\sshd"
+#Repair-AuthorizedKeyPermission $authorizedFile -Confirm:$false
+#icacls $authorizedFile /remove "NT SERVICE\sshd"
 
 $sshd = Get-Content -Path $ssh_config -Raw
 $sshd = $sshd.Replace("Match Group administrators", "#Match Group administrators")
